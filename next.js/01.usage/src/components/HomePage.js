64c7961c608b430c4233bf34a2f80b2b30f15700
Elif Çiftçi <eliffbat@gmail.com>
2022-11-13 15:12:38 +0300

    installation next.js

---
+++ next.js/01.usage/src/components/HomePage.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Header from "./Header";
+function HomePage() {
+  const names = ["Ada Lovelace", "Grace Hopper", "Margaret Hamilton"];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <Header title="Develop. Preview. Ship. 🚀" />
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
+
+export default HomePage;
